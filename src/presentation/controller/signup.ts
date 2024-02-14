ac107f09b2266e5faa750c7a4c7183f6c9dba8f5
Jose Martins <martins.josemn@gmail.com>
2024-02-14 01:56:26 -0300

    feat: ensure signUpController return 500 if if EmailValidator throws

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,30 +1,43 @@
-import { MissingParamError } from "../errors/missing-param-error"
-import { badRequest } from "../helpers/http-helper"
-import { HttpRequest, HttpResponse } from "../protocols/http"
-import { Controller } from "../protocols/controller"
+import { MissingParamError } from "../errors/missing-param-error";
+import { badRequest } from "../helpers/http-helper";
+import { HttpRequest, HttpResponse } from "../protocols/http";
+import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/email-validator";
 import { InvalidParamError } from "../errors/invalid-param-error";
+import { ServerError } from "../errors/server-error";
 
 export class SignUpController implements Controller {
-  private readonly emailValidator: EmailValidator
-  
-  constructor(emailValidator: EmailValidator){
-    this.emailValidator = emailValidator
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
   }
-  
+
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFieds = ['name', 'email', 'password', 'password_confirmation']
+    try {
+      const requiredFieds = [
+        "name",
+        "email",
+        "password",
+        "password_confirmation",
+      ];
 
-    for(const field of requiredFieds){
-      if(!httpRequest.body[field]){
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFieds) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
 
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    
-    if(!isValid){
-      return badRequest(new InvalidParamError('email'))
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"));
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
   }
 }
