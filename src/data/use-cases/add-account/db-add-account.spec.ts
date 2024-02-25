7b65f71141aa2042a97fdf215164bc21c17186ae
Jose Martins <martins.josemn@gmail.com>
2024-02-25 03:29:54 -0300

    test: ensure DbAddAccount throws if Encrypter throws

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -1,35 +1,35 @@
 import { Encrypter } from "../../protocols/encrypter";
 import { DbAddAccount } from "./db-add-account";
 
-interface SutTypes{
-  sut: DbAddAccount
-  encrypterStub: Encrypter
+interface SutTypes {
+  sut: DbAddAccount;
+  encrypterStub: Encrypter;
 }
 
-const makeEncrypter = (): Encrypter  => {
+const makeEncrypter = (): Encrypter => {
   class EncrypterStub implements Encrypter {
     async encrypt(value: string): Promise<string> {
       return new Promise((resolve) => resolve("valid_password"));
     }
   }
 
-  return new EncrypterStub()
-}
+  return new EncrypterStub();
+};
 
-const makeSut = (): SutTypes =>{
-  const encrypterStub =  makeEncrypter();
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter();
   const sut = new DbAddAccount(encrypterStub);
 
   return {
     sut,
-    encrypterStub
-  }
-}
+    encrypterStub,
+  };
+};
 
 describe("DbAddAccount UseCase ", () => {
   test("Should call Encrypter with correct password", async () => {
-    const {encrypterStub, sut} = makeSut()
-    const encryptSpy = jest.spyOn(encrypterStub , "encrypt");
+    const { encrypterStub, sut } = makeSut();
+    const encryptSpy = jest.spyOn(encrypterStub, "encrypt");
     const accountData = {
       name: "valid_name",
       email: "valid_email",
@@ -39,4 +39,21 @@ describe("DbAddAccount UseCase ", () => {
     await sut.add(accountData);
     expect(encryptSpy).toHaveBeenCalledWith("valid_password");
   });
+
+  test("Should throw if Encrypter throws", async () => {
+    const { encrypterStub, sut } = makeSut();
+    jest
+      .spyOn(encrypterStub, "encrypt")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    };
+
+    const promise = sut.add(accountData);
+    await expect(promise).rejects.toThrow();
+  }); 
 });
